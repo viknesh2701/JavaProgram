7b06d60e579bfe0a839f0957565048250f0f21a4
VIKNESH K <128950710+viknesh2701@users.noreply.github.com>
2023-07-24 15:02:11 +0530

    Add files via upload

---
+++ javaprogram/src/javaprogram/TestCase_6.java
@@ -0,0 +1,24 @@
+package TestCases;
+
+import java.util.Scanner;
+
+public class TestCase_6 {
+
+	public static void main(String[] args) {
+	Scanner s=new Scanner(System.in);
+	System.out.println("Enter:");
+	String str=s.nextLine();
+	char c[]=str.toCharArray();
+	for (int i=0; i<c.length; i++)
+	{
+		int t=c [i+1]-'0';
+		while(i-- >i) {
+			System.out.println(c);
+		}
+		
+	}
+	
+
+	}
+
+}
