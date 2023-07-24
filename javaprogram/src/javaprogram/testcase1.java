19b762628913250dcbc1f332be968459fa125c24
viknesh2701 <sanjaiv80@gmail.com>
2023-07-24 14:31:08 +0530

    Intial Commit.

---
+++ javaprogram/src/javaprogram/testcase1.java
@@ -0,0 +1,16 @@
+package javaprogram;
+
+public class testcase1 {
+	  public static void printLeftTriangle(int n) {
+          for (int i = 1; i <= n; i++) {
+              for (int j = 1; j <= i; j++) {
+                  System.out.print("* ");
+              }
+              System.out.println();
+          }
+      }
+      public static void main(String[] args) {
+          int n = 5;
+          printLeftTriangle(n);
+      }
+}
